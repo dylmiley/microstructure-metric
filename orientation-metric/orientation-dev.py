1d61f97c90095a1fad2d7ee5363b4604046014f3
dylmiley <137566839+dylmiley@users.noreply.github.com>
2025-07-22 15:36:57 -0700

    texture computation preliminary

--- orientation-metric/orientation-dev.py
+++ orientation-metric/orientation-dev.py
@@ -4,9 +4,6 @@ sys.path.append('../state-space/')
 import numpy as np
 import master_wass_metric_binary as mwmb
 
-rng = np.random.default_rng(12345)
-
-
 def quatmult(q1, q2):
     ### quaternion multiplication ### 
     ind1 = np.array([0, 1, 2, 3])
@@ -30,10 +27,44 @@ def misorientation(q1, q2):
     r = quatmult(q1, q2)
     return r
 
-
-if '__name__' == __main__:
-    l = 50        # px side length of windows
-    m = int(l**2) # px area of windows
-    iwB = mwmb.intrawindow_block(l)
+def texture_graph(A,B):
+    ### the graph for orientation difference for two windows, A and B ###
+    n = np.shape(A)[0]
+    G = np.zeros((n**2, n**2))
+    uniqueA = np.unique(A.reshape(-1,4), axis=0)
+    uniqueB = np.unique(B.reshape(-1,4), axis=0)
     
 
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                for l in range(n):
+                    for entry in rotation_list:
+                        if 
+                    G[]
+
+if '__name__' == __main__:
+    rng = np.random.default_rng()
+    
+    ### create transport landscape for a 5x5 window ###
+    l = 5         # px side length of windows
+    m = int(l**2) # px area of windows
+    iwB = mwmb.intrawindow_block(l)
+
+    ### create 2 random orientations ###
+    q1 = rng.random(size=4)
+    q1 = q1 / np.linalg.norm(q1)
+    q2 = rng.random(size=4)
+    q2 = q2 / np.linalg.norm(q2)
+
+    ### create two windows with randomly assigned q1 and q2 ###
+    A = np.ones((l,l,4))
+    A[:,:]*=q1
+    B = np.ones((l,l,4))
+    ijA = rng.integers(0, 5, size=(rng.integers(0,25), 2))
+    for ij in ijA:
+        A[ij[0], ij[1], :] = q2
+    ijB = rng.integers(0, 5, size=(rng.integers(0,25), 2))
+    for ij in ijB:
+        B[ij[0], ij[1], :] = q2
+    
